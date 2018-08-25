a6b918b7b75d59d3b4764d810761cafa1f2c2405
Masashi <qiuncyshin@yahoo.co.jp>
2018-08-25 11:49:02 +0900

    1 delete 2 modify

--- 2.py
+++ 2.py
@@ -4,16 +4,17 @@ from matplotlib import pylab as plt
 from create_vec import create_vec
 
 picmat = create_vec()
-
+print(picmat.shape)
 X_reduced = PCA(n_components=4).fit_transform(picmat)
 xi = 1 #X軸は第何主成分
 yi = 3 #Y軸は第何主成分
 X = X_reduced[0:160,xi] / 1000
 Y = X_reduced[0:160,yi] / 1000
 
-plt.scatter(X[0:31], Y[0:31],c='red',label='Anger')
-plt.scatter(X[32:63], Y[32:63],c='blue',label='Disgust')
-plt.scatter(X[64:95], Y[64:95],c='green',label='Fear')
-plt.scatter(X[96:127], Y[96:127],c='purple',label='Happy')
-plt.scatter(X[128:159], Y[128:159],c='cyan',label='Sadness')
+plt.scatter(X[0], Y[0],c='red',label='center')
+plt.scatter(X[1:31], Y[1:31],c='red',label='Anger')
+plt.scatter(X[32:62], Y[32:62],c='blue',label='Disgust')
+plt.scatter(X[63:93], Y[63:93],c='green',label='Fear')
+plt.scatter(X[94:124], Y[94:124],c='purple',label='Happy')
+plt.scatter(X[125:155], Y[125:155],c='cyan',label='Sadness')
 plt.show()
--- create_vec.py
+++ create_vec.py
@@ -11,16 +11,15 @@ def create_vec():
 
     picdir = pathlib.Path.cwd().parent / 'pictures/ver1/'
 
-    firstpic = np.reshape(np.array(Image.open(str(picdir)+'/000.jpg')),(-1,1))
+    firstpic = np.reshape(np.array(Image.open(str(picdir)+'/000.jpg')),(1,-1))
     picmat = firstpic
 
 
     for dirs in picdir.iterdir():
         if dirs.is_dir():
             for p in dirs.iterdir():
-                pic_tmp = np.reshape(np.array(Image.open(p)),(-1,1))
-                picmat = np.append(picmat,pic_tmp,axis=1)
-
+                pic_tmp = np.reshape(np.array(Image.open(p)),(1,-1))
+                picmat = np.append(picmat,pic_tmp,axis=0)
     return(picmat)
 
 
