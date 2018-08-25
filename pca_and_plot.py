ce07b6730f51b1fe981b9aad4396f0a09a276e14
Masashi <qiuncyshin@yahoo.co.jp>
2018-08-25 12:07:32 +0900

    2 delete and PCA program complete

--- pca_and_plot.py
+++ pca_and_plot.py
@@ -5,13 +5,17 @@ from create_vec import create_vec
 
 picmat = create_vec()
 print(picmat.shape)
+
+PCA(n_components=4).fit(picmat)
 X_reduced = PCA(n_components=4).fit_transform(picmat)
+
 xi = 1 #X軸は第何主成分
 yi = 3 #Y軸は第何主成分
-X = X_reduced[0:160,xi] / 1000
-Y = X_reduced[0:160,yi] / 1000
 
-plt.scatter(X[0], Y[0],c='red',label='center')
+X = (X_reduced[0:155,xi] - X_reduced[0,xi])/1000.0
+Y = (X_reduced[0:155,yi] - X_reduced[0,yi])/1000.0
+
+plt.scatter(X[0], Y[0],c='lime',label='center')
 plt.scatter(X[1:31], Y[1:31],c='red',label='Anger')
 plt.scatter(X[32:62], Y[32:62],c='blue',label='Disgust')
 plt.scatter(X[63:93], Y[63:93],c='green',label='Fear')
